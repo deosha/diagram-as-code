c079d76892d98ea2145ea7046451cccbfe4b6c0c
Deo Shankar <deo@Deos-MacBook-Pro.local>
2020-06-25 19:00:41 +0530

    added subnets

--- simple-three-tier-ecs-with-redis/diagram.py
+++ simple-three-tier-ecs-with-redis/diagram.py
@@ -6,26 +6,32 @@ from diagrams.aws.network import Route53
 
 with Diagram("architecture", show=False):
     dns = Route53("dns")
-    ext_lb = ELB("External ALB")
-    int_lb = ELB("Internal ALB")
+ 
+    with Cluster("Public Subnet"):
+        ext_lb = ELB("External ALB")
+    
+    with Cluster("Private Subnet"):
+        int_lb = ELB("Internal ALB")
 
-    with Cluster("Nginx-ECS"):
-        nginx = [ECS("Nginx"),
-                     ECS("Nginx"),
-                     ECS("Nginx")]
+        with Cluster("Nginx-ECS"):
+            nginx = [ECS("Nginx"),
+                        ECS("Nginx"),
+                        ECS("Nginx")]
 
-    with Cluster("API-ECS"):
-        backend = [ECS("Api"),
-                     ECS("Api"),
-                     ECS("Api")]
+        with Cluster("API-ECS"):
+            backend = [ECS("Api"),
+                         ECS("Api"),
+                         ECS("Api")]
 
-    with Cluster("DB Cluster"):
-        db = RDS("Master")
-        db - [RDS("Slave")]
+    with Cluster("Database Subnet"): 
 
-    with Cluster("Redis Sentinel"):
-        redis = EC2("node1")
-        redis - [EC2("node2"),EC2("node3")]
+        with Cluster("DB Cluster"):
+            db = RDS("Master")
+            db - [RDS("Slave")]
+
+        with Cluster("Redis Sentinel"):
+            redis = EC2("node1")
+            redis - [EC2("node2"),EC2("node3")]
 
 
     dns >> ext_lb >> nginx
